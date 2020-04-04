540bd59642d64a27ecca000ead80dbad73738d9a
Alfredo Miranda <alfredo@miranda.io>
2020-04-04 11:15:25 -0500

    Add all fields needed for the known drugs table

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const knownDrugsData = JSON.parse(fs.readFileSync('./ESR1-known-drugs.json', 'ut
 
 const typeDefs = gql`
   type Query {
-    knownDrugs(page: Pagination): KnownDrugs
+    knownDrugs(page: Pagination, sort: SortInput): KnownDrugs
   }
 
   input Pagination {
     index: Int!
     size: Int!
+  }
+
+  input SortInput {
+    sortBy: String!
+    direction: String
   }
 
   type KnownDrugs {
@@ -26,39 +31,88 @@ const typeDefs = gql`
   type KnownDrug {
     disease: String!
     phase: Int!
+    status: String!
+    source: String!
+    drug: String!
+    type: String!
+    mechanism: String!
+    activity: String!
   }
 `;
 
+function sortDrugs(data, sort) {
+  if (!sort) return data;
+
+  const { sortBy, direction } = sort;
+
+  return data.sort((a, b) => {
+    if (direction === 'asc') {
+      if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+
+      return 0;
+    } else {
+      if (a[sortBy] < b[sortBy]) {
+        return 1;
+      }
+
+      if (a[sortBy] > b[sortBy]) {
+        return -1;
+      }
+
+      return 0;
+    }
+  });
+}
+
+function getPage(data, page) {
+  if (page) {
+    const i = page.size * page.index;
+    return knownDrugsData.slice(i, i + page.size).map(d => {
+      return {
+        disease: d.disease,
+        phase: d.phase,
+        status: d.status,
+        source: d.source,
+        drug: d.drug,
+        type: d.type,
+        mechanism: d.mechanism,
+        activity: d.activity
+      };
+    })
+  }
+
+  return knownDrugsData.slice(0, 10).map(d => {
+    return {
+      disease: d.disease,
+      phase: d.phase,
+      status: d.status,
+      source: d.source,
+      drug: d.drug,
+      type: d.type,
+      mechanism: d.mechanism,
+      activity: d.activity
+    };
+  });
+}
+
 const resolvers = {
   Query: {
-    knownDrugs(_, { page }) {
+    knownDrugs(_, { page, sort }) {
+      let drugs = sortDrugs(knownDrugsData, sort);
+      drugs = getPage(drugs, page);
 
-      if (page) {
-        const i = page.size * page.index;
-        return {
-          aggregations: {
-            total: knownDrugsData.length,
-          },
-          rows: knownDrugsData.slice(i, i + page.size).map(d => {
-            return {
-              disease: d.disease,
-              phase: d.phase
-            };
-          })
-        };
-      } else {
-        return {
-          aggregations: {
-            total: knownDrugsData.length
-          },
-          rows: knownDrugsData.slice(0, 10).map(d => {
-            return {
-              disease: d.disease,
-              phase: d.phase
-            };
-          })
-        }
-      }
+      return {
+        aggregations: {
+          total: knownDrugsData.length
+        },
+        rows: drugs
+      };
     }
   }
 };
