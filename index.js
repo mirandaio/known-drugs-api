4fe7852fda3e5e78281cce9c3c571b1a7579975e
Alfredo Miranda <alfredo@miranda.io>
2020-04-04 20:30:02 -0500

    Add filters for all fields

--- index.js
+++ index.js
@@ -113,24 +113,54 @@ function getPage(data, page) {
   });
 }
 
-const options = {
-  isCaseSensitive: false,
-  // minMatchCharLength: 5,
-  threshold: 0.6,
-  keys: [ 'disease' ]
-};
-
 function filter(data, filters) {
+  if (!filters) return data;
+
   const {
     disease, phase, status, source, drug, type, mechanism, activity
   } = filters;
+
 
   let filteredData = data;
 
 
   if (disease) {
-    const fuseDisease = new Fuse(filteredData, options);
+    const fuseDisease = new Fuse(filteredData, { keys: [ 'disease' ]});
     filteredData = fuseDisease.search(disease).map(d => d.item);
+  }
+
+  if (phase != undefined) {
+    filteredData = filteredData.filter(d => d.phase === phase);
+  }
+
+  if (status) {
+    const fuseStatus = new Fuse(filteredData, { keys: [ 'status' ] });
+    filteredData = fuseStatus.search(status).map(d => d.item);
+  }
+
+  if (source) {
+    const fuseSource = new Fuse(filteredData, { keys: [ 'source' ] });
+    filteredData = fuseSource.search(source).map(d => d.item);
+  }
+
+  if (drug) {
+    const fuseDrug = new Fuse(filteredData, { keys: [ 'drug' ] });
+    filteredData = fuseDrug.search(drug).map(d => d.item);
+  }
+
+  if (type) {
+    const fuseType = new Fuse(filteredData, { keys: [ 'type' ] });
+    filteredData = fuseType.search(type).map(d => d.item);
+  }
+
+  if (mechanism) {
+    const fuseMechanism = new Fuse(filteredData, { keys: [ 'mechanism' ] });
+    filteredData = fuseMechanism.search(mechanism).map(d => d.item);
+  }
+
+  if (activity) {
+    const fuseActivity = new Fuse(filteredData, { keys: [ 'activity' ] });
+    filteredData = fuseActivity.search(activity).map(d => d.item);
   }
 
   return filteredData;
